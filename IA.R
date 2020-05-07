15739bf0b06462571322524011ac91652bc25c72
Basa84 <bsauze@etu.isara.fr>
2020-05-07 10:26:53 +0200

    graphiques PCA

--- IA.R
+++ IA.R
@@ -2,16 +2,17 @@ library(ggplot2)
 library(babynames) # provide the dataset: a dataframe called babynames
 library(dplyr)
 
-#models <- read.csv("models.csv")
+models <- read.csv("models.csv")
 
-#models$Nom <-paste(models$Nombre.de.neurones.dans.la.couche.complètement.connectée,models$Dataset.d.entraînement,sep=".")
+models$Nom <-paste(models$Nombre.de.neurones.dans.la.couche.complètement.connectée,models$Dataset.d.entraînement,sep=".")
 
-filter(models, Nom %in% c("128. catdog", "128.savane50", "128savane100", "512.savane100"))
+# filter(models, Nom %in% c("128. catdog", "128.savane50", "128savane100", "512.savane100"))
+# 
+# # Plot
+# models %>%
+#   ggplot( aes(x=Nombre.passes, y=Pourcentage.de.réussite, group=Nom, color=Nom)) +
+#   geom_line() +
+# #  scale_color_viridis(discrete = TRUE) +
+#   ggtitle("% de réussite en fonction du nombre de passes") +
+#   ylab("% de réussites")
 
-# Plot
-models %>%
-  ggplot( aes(x=Nombre.passes, y=Pourcentage.de.réussite, group=Nom, color=Nom)) +
-  geom_line() +
-#  scale_color_viridis(discrete = TRUE) +
-  ggtitle("% de réussite en fonction du nombre de passes") +
-  ylab("% de réussites")
