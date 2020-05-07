f1f159b2823d718f6a75b78d660c01f3067a10b3
Basa84 <bsauze@etu.isara.fr>
2020-05-07 12:01:50 +0200

    commentaires sur graph 1 et 2

--- IA.R
+++ IA.R
@@ -9,27 +9,41 @@ models <- read.csv("models.csv")
 models$Dataset.d.entraînement <- NULL
 models$Dataset.de.test <- NULL
 
-#models$Nom <-paste(models$Noinstall.packages(c("FactoMineR", "factoextra"))mbre.de.neurones.dans.la.couche.complètement.connectée,models$Dataset.d.entraînement,sep=".")
+models$Nom <-paste(models$Nombre.de.neurones.dans.la.couche.complètement.connectée,models$Dataset.d.entraînement,sep=".")
 
-# filter(models, Nom %in% c("128. catdog", "128.savane50", "128savane100", "512.savane100"))
-# 
-# # Plot
-# models %>%
-#   ggplot( aes(x=Nombre.passes, y=Pourcentage.de.réussite, group=Nom, color=Nom)) +
-#   geom_line() +
-# #  scale_color_viridis(discrete = TRUE) +
-#   ggtitle("% de réussite en fonction du nombre de passes") +
-#   ylab("% de réussites")
+filter(models, Nombre.de.classes %in% c("9", "2"))
+ 
+ # Plot
+ models %>%
+   ggplot( aes(x=Nombre.de.classes, y=Pourcentage.de.réussite, group=Nombre.de.classes, color=Nombre.de.classes)) +
+   geom_line() +
+   ggtitle("% de réussite en fonction du nombre de passes") +
+   ylab("% de réussites")
 
-res.pca <- PCA(models, graph = TRUE)
+ op <- par(bg = "lightgrey", fg = "white", lwd = 2)
+ 
+ plot(models$Nombre.de.classes, models$Pourcentage.de.réussite,  
+      pch = 20, cex = 0.8, col = "red", 
+      xlab = "Nombre de classes",
+      ylab = "% de réussite")
+ 
+ lines(lowess(models$Nombre.de.classes, models$Pourcentage.de.réussite), col = "black")
+ title("% de réussite en fonction du nombre de classes")  
+ 
 
-eig.val <- res.pca$eig
-
-plot(res.pca, choix = "ind", autoLab = "yes")
-
-fviz_eig(res.pca, addlabels = TRUE, ylim = c(0, 60))
-
-# Contributions des variables à PC1
-fviz_contrib(res.pca, choice = "var", axes = 1, top = 10)
-# Contributions des variables à PC2
-fviz_contrib(res.pca, choice = "var", axes = 2, top = 10)
+ res.pca <- PCA(models, graph = TRUE)
+ 
+ eig.val <- res.pca$eig
+ 
+ plot(res.pca, choix = "ind", autoLab = "yes")
+ 
+ fviz_eig(res.pca, addlabels = TRUE, ylim = c(0, 60))
+ 
+ # Contributions des variables à PC1
+ fviz_contrib(res.pca, choice = "var", axes = 1, top = 10)
+ # Contributions des variables à PC2
+ fviz_contrib(res.pca, choice = "var", axes = 2, top = 10)
+ fviz_contrib(res.pca, choice = "var", axes = 2, top = 10)
+ fviz_pca_var(res.pca, col.var = "black", title="ACP des corrélations entre les variables")
+ 
+ 
