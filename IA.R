0030808542a2fa752d6602586eed35090388d1c4
Basa84 <bsauze@etu.isara.fr>
2020-05-07 10:41:07 +0200

    ajout du cercle des corrélations

--- IA.R
+++ IA.R
@@ -1,10 +1,15 @@
 library(ggplot2)
 library(babynames) # provide the dataset: a dataframe called babynames
 library(dplyr)
+library("FactoMineR")
+library("factoextra")
 
 models <- read.csv("models.csv")
 
-models$Nom <-paste(models$Nombre.de.neurones.dans.la.couche.complètement.connectée,models$Dataset.d.entraînement,sep=".")
+models$Dataset.d.entraînement <- NULL
+models$Dataset.de.test <- NULL
+
+#models$Nom <-paste(models$Noinstall.packages(c("FactoMineR", "factoextra"))mbre.de.neurones.dans.la.couche.complètement.connectée,models$Dataset.d.entraînement,sep=".")
 
 # filter(models, Nom %in% c("128. catdog", "128.savane50", "128savane100", "512.savane100"))
 # 
@@ -16,3 +21,15 @@ models$Nom <-paste(models$Nombre.de.neurones.dans.la.couche.complètement.connect
 #   ggtitle("% de réussite en fonction du nombre de passes") +
 #   ylab("% de réussites")
 
+res.pca <- PCA(models, graph = TRUE)
+
+eig.val <- res.pca$eig
+
+plot(res.pca, choix = "ind", autoLab = "yes")
+
+fviz_eig(res.pca, addlabels = TRUE, ylim = c(0, 60))
+
+# Contributions des variables à PC1
+fviz_contrib(res.pca, choice = "var", axes = 1, top = 10)
+# Contributions des variables à PC2
+fviz_contrib(res.pca, choice = "var", axes = 2, top = 10)
